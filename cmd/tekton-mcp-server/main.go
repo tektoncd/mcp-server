4ef2bebbad0f263f949ef2c9e6d727da3ff1e3a5
Vincent Demeester <vincent@sbr.pm>
2025-04-23 16:56:21 +0200

    Add knative.dev/pkg and mcp-go dependencies
    This starts a dummy binary that creates an mcp server but empty.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- cmd/tekton-mcp-server/main.go
+++ cmd/tekton-mcp-server/main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/mark3labs/mcp-go/server"
+	"knative.dev/pkg/signals"
 )
 
 func main() {
-	fmt.Println("Hello tektoncd/mcp-server")
+	// Create MCP server
+	s := server.NewMCPServer(
+		"Tekton",
+		"0.0.1", // FIXME get this from internal package
+		server.WithResourceCapabilities(true, true),
+		server.WithPromptCapabilities(true),
+		server.WithToolCapabilities(true),
+		server.WithLogging(),
+	)
+
+	ctx := signals.NewContext()
+
+	slog.Info("Starting the server.")
+	// Start the stdio server
+	stdioServer := server.NewStdioServer(s)
+	// Start listening for messages
+	errC := make(chan error, 1)
+	go func() {
+		in, out := io.Reader(os.Stdin), io.Writer(os.Stdout)
+
+		errC <- stdioServer.Listen(ctx, in, out)
+	}()
+
+	// Output tekton-mcp string
+	_, _ = fmt.Fprintf(os.Stderr, "Tekton MCP Server running on stdio\n")
+
+	// Wait for shutdown signal
+	select {
+	case <-ctx.Done():
+		slog.Info("Shutting down server...")
+	case err := <-errC:
+		if err != nil {
+			slog.Error(fmt.Sprintf("Error running server: %v", err))
+			os.Exit(1)
+		}
+	}
 }
