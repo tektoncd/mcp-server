b2308b3ee7b6657f960a00793143cd50aea9ec01
Brady Pratt <bpratt@redhat.com>
2025-04-25 09:11:15 -0500

    tools: generate json schema from openapi def
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

---
+++ internal/tools/schema.go
@@ -0,0 +1,43 @@
+package tools
+
+import (
+	"encoding/json"
+
+	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+	common "k8s.io/kube-openapi/pkg/common"
+	"k8s.io/kube-openapi/pkg/validation/spec"
+)
+
+func getSchemaForType(name string) string {
+	openapiDefinitions := v1.GetOpenAPIDefinitions(
+		func(path string) spec.Ref {
+			return spec.MustCreateRef(path)
+		},
+	)
+	definition, ok := openapiDefinitions[name]
+	if !ok {
+		return ""
+	}
+	resolvedDefinition := resolveReferences(definition.Schema, openapiDefinitions)
+	data, _ := json.Marshal(resolvedDefinition)
+	return string(data)
+}
+
+func resolveReferences(schema spec.Schema, definitions map[string]common.OpenAPIDefinition) spec.Schema {
+	if schema.Type.Contains("object") {
+		for name, prop := range schema.Properties {
+			schema.Properties[name] = resolveReferences(prop, definitions)
+		}
+	} else if schema.Type.Contains("array") {
+		resolvedSchemas := make([]spec.Schema, 0)
+		for _, sch := range schema.Items.Schemas {
+			resolvedSchemas = append(resolvedSchemas, resolveReferences(sch, definitions))
+		}
+		schema.Items.Schemas = resolvedSchemas
+	}
+	if path := schema.Ref.String(); path != "" {
+		definition := definitions[path]
+		return resolveReferences(definition.Schema, definitions)
+	}
+	return schema
+}
