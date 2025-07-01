92848e8dad6c050b60eb5ac1e25c33e4e891a765
Brady Pratt <bpratt@redhat.com>
2025-06-30 19:24:03 -0500

    internal/tools: refactor tests to use InMemoryTransports
    we can simply call the tools directly from the server now with the in
    memory transport to validate schema and server creation in the tests
    
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

---
+++ internal/tools/tools_test.go
@@ -0,0 +1,32 @@
+package tools
+
+import (
+	"context"
+	"testing"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+	"github.com/tektoncd/mcp-server/internal/resources"
+	"github.com/tektoncd/mcp-server/internal/version"
+)
+
+func newSession(t *testing.T, ctx context.Context) (*mcp.ServerSession, *mcp.ClientSession) {
+	t.Helper()
+
+	ct, st := mcp.NewInMemoryTransports()
+	s := mcp.NewServer("Tekton", version.Version, nil)
+	Add(ctx, s)
+	resources.Add(ctx, s)
+	c := mcp.NewClient("TektonClient", version.Version, nil)
+
+	ss, err := s.Connect(ctx, st)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cs, err := c.Connect(ctx, ct)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return ss, cs
+}
